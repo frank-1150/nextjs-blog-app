b7328ca0de89172d4cd54d7082319a1d954ddd04
Frank <luozhh1024@g.ucla.edu>
2023-03-30 15:40:39 -0700

    Modify index page's introduction

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,12 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          I am a software engineer specialized in building web applications
+          using Next.JS framework
+        </p>
+        <p>
+          The following are sample blogs that can be any other articles written.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
